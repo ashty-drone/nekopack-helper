7c1cc90baf93979da1d8ffd37be1320b218560e1
ashty-drone <77475908+ashty-drone@users.noreply.github.com>
2022-07-19 22:08:11 +0530

    Update postgres.sh

--- postgres.sh
+++ postgres.sh
@@ -1,2 +1,2 @@
-su - postgres -c "psql -c \"ALTER USER postgres WITH PASSWORD 'seckry';\""
+su - postgres -c "psql -c \"ALTER USER postgres WITH PASSWORD '#';\""
 su - postgres -c "psql -c 'CREATE DATABASE catuserbot'"
