7ef976e57cefec5422f761fee51ff81270f1e8d6
ashty-drone <77475908+ashty-drone@users.noreply.github.com>
2022-08-08 12:10:41 +0530

    Update _postgres.py

--- _postgres.py
+++ _postgres.py
@@ -3,6 +3,8 @@ from telethon.sessions import StringSession
 from telethon import TelegramClient, functions
 from telethon.tl.types import InputMessagesFilterDocument as document
 
+print("check 0")
+
 async def getBackupChannels():
   async def get_backup_channel():
     async with client.takeout(finalize=True) as takeout: #credits to https://t.me/UniBorg/142
@@ -11,6 +13,7 @@ async def getBackupChannels():
     for chat in result.chats:
       if chat.title == 'CatUserbot Database Backup Group':
         channel_id.append(chat.id)
+    print("check 1")
     return channel_id
   try: await get_back_channel()
   except: return None
@@ -20,6 +23,7 @@ async def joinBackupChannels():
   if not channel_id: return channel_id
   for chat in channel_id:
     await client(functions.channels.JoinChannelRequest(channel=chat))
+    print("check 2")
   return channel_id
   
 async def getBackupSQL():
@@ -30,6 +34,7 @@ async def getBackupSQL():
           date = message.date
           if download:
             download = await client.download_media(message)
+            print("check 5")
             return
           return date
     try: await iterBackupSql(chat, download=download)
@@ -40,11 +45,13 @@ async def getBackupSQL():
   channel_id = getenv("CATUSERBOT_DATABASE_GROUP_ID")
   if channel_id:
     iter_backup_sql(int((str(channel_id)).strip()), download=True)
+    print("check 3"
   else:
     channel_id = await joinBackupChannels()
     if channel_id:
       if len(channel_id) == 1:
         iter_backup_sql(channel_id, download=True)
+        print("check 4")
       else:
         date = []
         for chat in channel_id:
